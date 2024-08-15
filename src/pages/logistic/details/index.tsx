6ecb2da3927350926259e27318d66e086133c064
Calvin <calvinsoares17@gmail.com>
2024-08-15 07:09:18 -0300

    feat: base componentes

--- src/pages/logistic/details/index.tsx
+++ src/pages/logistic/details/index.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import Card from "../../../shared/components/card"
+import Table from "../../../shared/components/table"
 
 const DetailsLogistic = () => {
   return (
-    <div>
-      details
+    <div className="w-full h-full">
+     <div className="flex gap-4 py-12">
+      <Card count={12} description="lorem dasjdakjds a da,mdawdmdmasdmsm dawmdmamdsa" text="clientes" link="/registerclient"/>
+      <div className="bg-white flex justify-start items-start w-[20%] h-[16%] rounded-3xl ">
+          dsds  
+      </div>
+     </div>
+      <Table />
     </div>
   )
 }
