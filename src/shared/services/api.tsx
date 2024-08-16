b3f866f2b903aafc1da2d0b8b1d7ceaf50d13794
Calvin <calvinsoares17@gmail.com>
2024-08-16 18:23:53 -0300

    feat: baseUrl chagend

--- src/shared/services/api.tsx
+++ src/shared/services/api.tsx
@@ -1,9 +1,13 @@
 import axios from "axios";
 
+
+const token = localStorage.getItem("token")
+
 const api = axios.create({
     baseURL: 'http://localhost:4002',
     headers: {
       'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token}`
     },
   });
 
