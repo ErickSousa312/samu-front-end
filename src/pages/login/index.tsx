66950d7876daf864d92e7a349d539c4711822411
Calvin <calvinsoares17@gmail.com>
2024-08-17 18:54:42 -0300

    feat: addToast context for notifications

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -2,7 +2,7 @@ import { useForm, SubmitHandler } from 'react-hook-form';
 import { useAuth } from '../../shared/context/AuthContext/AuthProvider';
 import api from '../../shared/services/api';
 import { useNavigate } from 'react-router-dom';
-import { ToastProvider, useToast } from '../../shared/context/ToastContext';
+import { useToast } from '../../shared/context/ToastContext';
 
 interface LoginFormInputs {
   email: string;
@@ -29,14 +29,11 @@ const LoginPage = () => {
       
     } catch (error) {
       addToast({ type: 'error', message: 'Falha no login. Verifique suas credenciais.' });
-
       console.error('Login failed:', error);
     }
   };
 
   return (
-
-    <ToastProvider>
     <div className='w-screen h-screen m-auto'>
  <div className="relative flex h-screen  text-gray-100 antialiased flex-col justify-center overflow-hidden bg-[#121212] py-6 sm:py-12">
         <div className="relative py-3 sm:w-96 mx-auto text-center">
@@ -56,8 +53,6 @@ const LoginPage = () => {
         </div>
       </div>
     </div>
-    </ToastProvider>
-
    
   );
 };
