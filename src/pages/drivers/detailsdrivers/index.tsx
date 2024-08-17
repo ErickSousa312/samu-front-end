cfc35a384368e625d1bed6b020c142e6b04177ff
Calvin <calvinsoares17@gmail.com>
2024-08-17 18:53:17 -0300

    feat: table with role implemented

--- src/pages/drivers/detailsdrivers/index.tsx
+++ src/pages/drivers/detailsdrivers/index.tsx
@@ -3,8 +3,8 @@ import Table from '../../../shared/components/table'
 
 const DetailsDrivers = () => {
   return (
-    <div className='bg-[#161617] pt-12'>
-      <Table role="driver" />
+    <div className='bg-[#161617] pt-12 w-full'>
+      <Table role='driver' />
     </div>
   )
 }
