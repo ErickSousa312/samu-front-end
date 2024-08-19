fa988954eae5babdcbed2bf9f7f55373bc730ab6
ErickSousa312 <58636303+ErickSousa312@users.noreply.github.com>
2024-08-19 17:04:47 -0300

    update pdf link 4

--- src/shared/services/api.tsx
+++ src/shared/services/api.tsx
@@ -4,7 +4,7 @@ import axios from "axios";
 const token = localStorage.getItem("token")
 
 const api = axios.create({
-    baseURL: 'https://api-logistic-1.onrender.com/',
+    baseURL: 'https://libras.helpdesk-maraba.cloud/',
     headers: {
       'Content-Type': 'application/json',
       'Authorization': `Bearer ${token}`
