3547a7e2b64aab7b6d1559ace5f3a4c612d3f106
Calvin <calvinsoares17@gmail.com>
2024-08-25 00:55:34 -0300

    feat: new input date

--- src/pages/orders/modalCreateOrder/index.tsx
+++ src/pages/orders/modalCreateOrder/index.tsx
@@ -3,7 +3,7 @@ import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import api from "../../../shared/services/api";
 import { useToast } from "../../../shared/context/ToastContext";
-import { useState, useEffect } from "react";
+import { useState, useEffect } from "react"; 
 import Dropdown from "../../../shared/components/select";
 
 interface CreateOrderProps {
@@ -18,27 +18,26 @@ const orderSchema = z.object({
   plano: z.string().min(1, "Plano é obrigatório"),
   phone: z.string().min(1, "Telefone é obrigatório"),
   email: z.string().email("Email inválido").optional(),
-  userId: z.string().optional(), 
+  userId: z.string().optional(),
+  deliveryDate: z.string().optional(),  // Transformação aqui
 });
 
 type OrderFormData = z.infer<typeof orderSchema>;
 
 type InputField = 
   | { placeholder: string; type: "text" | "number" | "email"; }
-  | { placeholder: string; type: "select"; options: string[]; };
+  | { placeholder: string; type: "select"; options: string[]; }
+  | { placeholder: string; type: "date"; };
 
 const inputFields: Record<string, InputField> = {
-  status: { placeholder: "Status", type: "select", options: [
-    "Entregue",
-    "Pendente",
-    "Cancelado"
-  ] },
+  status: { placeholder: "Status", type: "select", options: ["Entregue", "Pendente", "Cancelado"] },
   email: { placeholder: "Email", type: "email" },
   userName: { placeholder: "Nome", type: "text" },
   address: { placeholder: "Endereço", type: "text" },
   price: { placeholder: "Preço", type: "number" },
-  plano: { placeholder: "Plano", type: "text" },
+  plano: { placeholder: "Plano", type: "select", options: ["Mercado Livre Flex", "Mercado Livre Flex Turbo", "Serviços Particular"] },
   phone: { placeholder: "Telefone", type: "text" },
+  deliveryDate: { placeholder: "Data", type: "date" }, // Campo de data
 };
 
 const CreateOrder = ({ onClose }: CreateOrderProps) => {
@@ -54,6 +53,9 @@ const CreateOrder = ({ onClose }: CreateOrderProps) => {
       setValue,
     } = useForm<OrderFormData>({
       resolver: zodResolver(orderSchema),
+      defaultValues: {
+        deliveryDate: new Date().toISOString(), // Ensure a default value is set
+      },
     });
 
     useEffect(() => {
@@ -71,32 +73,37 @@ const CreateOrder = ({ onClose }: CreateOrderProps) => {
     
       fetchUsers();
     }, []);
-    
 
-   const handleUserSelect = (user: { email: string; id: string }) => {
-    setSelectedUser(user);
-    setValue("email", user.email);
-    setValue("userId", user.id); 
-  };
-  const onSubmit = async (data: OrderFormData) => {
-    try {
-      if (!selectedUser) {
-        throw new Error("Usuário deve ser selecionado");
+    const handleChangeDate = (e: React.ChangeEvent<HTMLInputElement>) => {
+      const date = e.target.value;
+      setValue("deliveryDate", date); // Set ISO string directly
+    };
+
+    const handleUserSelect = (user: { email: string; id: string }) => {
+      setSelectedUser(user);
+      setValue("email", user.email);
+      setValue("userId", user.id); 
+    };
+
+    const onSubmit = async (data: OrderFormData) => {
+      try {
+        if (!selectedUser) {
+          throw new Error("Usuário deve ser selecionado");
+        }
+  
+        const formData = { ...data, userId: selectedUser.id };
+        console.log("Dados enviados:", formData);
+  
+        addToast({ type: 'loading', message: 'Aguarde, estamos processando seu pedido...' });
+  
+        await api.post(`/orders`, formData);
+        addToast({ type: 'success', message: 'Pedido criado com sucesso!' });
+        onClose();
+      } catch (err) {
+        addToast({ type: 'error', message: 'Falha ao criar pedido.' });
+        console.error("Erro na criação de pedidos", err);
       }
-  
-      const formData = { ...data, userId: selectedUser.id };
-      console.log("Dados enviados:", formData);
-  
-      addToast({ type: 'loading', message: 'Aguarde, estamos processando seu pedido...' });
-  
-      await api.post(`/orders`, formData);
-      addToast({ type: 'success', message: 'Pedido criado com sucesso!' });
-      onClose();
-    } catch (err) {
-      addToast({ type: 'error', message: 'Falha ao criar pedido.' });
-      console.error("Erro na criação de pedidos", err);
-    }
-  };
+    };
 
     return (
       <div className="fixed inset-0 z-50 flex items-center justify-center bg-gray-900/50">
@@ -108,79 +115,79 @@ const CreateOrder = ({ onClose }: CreateOrderProps) => {
                 <h1 className="text-2xl font-bold text-white">Criar Pedido</h1>
               </div>
               <div className="mt-4 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
-              <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col justify-center items-center">
-                <div className="grid grid-cols-2 gap-8">
-                  {Object.entries(inputFields).map(([name, field]) => (
-                    <div key={name} className="">
-                      {field.type === "select" ? (
-                        <div>
-                          <select
-                            {...register(name as keyof OrderFormData)}
-                            className={`mt-1 bg-transparent text-white p-2 border rounded w-[14vw] ${errors[name as keyof OrderFormData] ? 'border-red-500' : ''}`}
-                          >
-                            <option value="" disabled>
-                              {field.placeholder}
-                            </option>
-                            {field.options?.map((option) => (
-                              <option className="text-black" key={option} value={option}>
-                                {option}
+                <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col justify-center items-center">
+                  <div className="grid grid-cols-2 gap-8">
+                    {Object.entries(inputFields).map(([name, field]) => (
+                      <div key={name} className="">
+                        {field.type === "select" ? (
+                          <div>
+                            <select
+                              {...register(name as keyof OrderFormData)}
+                              className={`mt-1 bg-transparent text-white p-2 border rounded w-[14vw] ${errors[name as keyof OrderFormData] ? 'border-red-500' : ''}`}
+                            >
+                              <option value="" disabled>
+                                {field.placeholder}
                               </option>
-                            ))}
-                          </select>
-                          {errors[name as keyof OrderFormData] && (
-                            <p className="text-red-500 text-sm mt-1">{errors[name as keyof OrderFormData]?.message}</p>
-                          )}
-                        </div>
-                      ) : name === "email" ? (
-                        <div>
-                          <Dropdown
-                            name={selectedUser ? selectedUser.email : "Selecione um usuário"}
-                            options={users}
-                            onSelect={handleUserSelect}
-                            initialSelectedItem={selectedUser ?? undefined}
-                            loading={loading} 
-                          />
-                          {errors[name as keyof OrderFormData] && (
-                            <p className="text-red-500 text-sm mt-1">{errors[name as keyof OrderFormData]?.message}</p>
-                          )}
-                        </div>
-                      ) : (
-                        <div>
-                          <input
-                            type={field.type}
-                            placeholder={field.placeholder}
-                            {...register(name as keyof OrderFormData,
-                              name === "price" ? { valueAsNumber: true } : {}
+                              {field.options?.map((option) => (
+                                <option className="text-black" key={option} value={option}>
+                                  {option}
+                                </option>
+                              ))}
+                            </select>
+                            {errors[name as keyof OrderFormData] && (
+                              <p className="text-red-500 text-sm mt-1">{errors[name as keyof OrderFormData]?.message}</p>
                             )}
-                            className={`mt-1 w-[14vw] bg-transparent text-white p-2 border rounded ${errors[name as keyof OrderFormData] ? 'border-red-500' : ''}`}
-                          />
-                          {errors[name as keyof OrderFormData] && (
-                            <p className="text-red-500 text-sm mt-1">{errors[name as keyof OrderFormData]?.message}</p>
-                          )}
-                        </div>
-                      )}
-                    </div>
-                  ))}
-                </div>
-                <div className="pt-8 w-full gap-4 flex justify-between items-center">
-                  <button onClick={onClose} className="bg-gray-500 text-black px-4 py-2 rounded hover:bg-gray-600">
-                    Fechar
-                  </button>
-                  <button
-                    type="submit"
-                    className="bg-yellow-500 text-black px-4 py-2 rounded hover:bg-yellow-600"
-                  >
-                    Salvar
-                  </button>
-                </div>
-              </form>
-
+                          </div>
+                        ) : name === "email" ? (
+                          <div>
+                            <Dropdown
+                              name={selectedUser ? selectedUser.email : "Selecione um usuário"}
+                              options={users}
+                              onSelect={handleUserSelect}
+                              initialSelectedItem={selectedUser ?? undefined}
+                              loading={loading} 
+                            />
+                            {errors[name as keyof OrderFormData] && (
+                              <p className="text-red-500 text-sm mt-1">{errors[name as keyof OrderFormData]?.message}</p>
+                            )}
+                          </div>
+                        ) : (
+                          <div>
+                            <input
+                              type={field.type}
+                              placeholder={field.placeholder}
+                              {...register(name as keyof OrderFormData,
+                                name === "price" ? { valueAsNumber: true } : {}
+                              )}
+                              onChange={name === "deliveryDate" ? handleChangeDate : undefined}
+                              className={`mt-1 w-[14vw] bg-transparent text-white p-2 border rounded ${errors[name as keyof OrderFormData] ? 'border-red-500' : ''}`}
+                            />
+                            {errors[name as keyof OrderFormData] && (
+                              <p className="text-red-500 text-sm mt-1">{errors[name as keyof OrderFormData]?.message}</p>
+                            )}
+                          </div>
+                        )}
+                      </div>
+                    ))}
+                  </div>
+                  <div className="pt-8 w-full gap-4 flex justify-between items-center">
+                    <button onClick={onClose} className="bg-gray-500 text-black px-4 py-2 rounded hover:bg-gray-600">
+                      Fechar
+                    </button>
+                    <button
+                      type="submit"
+                      className="bg-yellow-500 text-black px-4 py-2 rounded hover:bg-yellow-600"
+                    >
+                      Salvar
+                    </button>
+                  </div>
+                </form>
               </div>
             </div>
           </div>
         </div>
       </div>
     );
-  };
-  
+};
+
 export default CreateOrder;
